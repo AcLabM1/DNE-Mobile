dc9fa20cf564f9deced98780606f7214b8ea1c3f
Tristan <mytic12@hotmail.fr>
2021-01-21 14:36:05 +0100

    clean code

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { View, Text, Button, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { getSessionData, setSessionData } from "../api/session";
 import { getUsernameByID } from "../api/mock";
 
@@ -26,102 +27,54 @@ const HomeScreen = ({ navigation }) => {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>HomeScreen</Text>
             <Text>Bonjour {getUsernameByID(userID)}</Text>
             <Button title="Déconnexion" onPress={() => logout()} />
             <View style={{ flexDirection: "row", paddingBottom:50 }}>
-            <View style={{paddingRight:50}}>
-            <TouchableOpacity
-                style={{
-                    borderWidth:1,
-                    borderColor:'rgba(0,0,0,0.2)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:100,
-                    height:100,
-                    backgroundColor:'#fff',
-                    borderRadius:50,
-                }}>
-                <Icon name={"chevron-right"}  size={30} color="#01a699" />
-            </TouchableOpacity>
-            </View>
-            <TouchableOpacity
-                style={{
-                    borderWidth:1,
-                    borderColor:'rgba(0,0,0,0.2)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:100,
-                    height:100,
-                    backgroundColor:'#fff',
-                    borderRadius:50
-                }}>
-                <Icon name={"chevron-right"}  size={30} color="#01a699" />
-            </TouchableOpacity>
+                <View style={{paddingRight:50}}>
+                    <TouchableOpacity style={styles.circle_button}>
+                        <FontAwesome name={"chevron-right"}  size={30} color="#01a699" />
+                    </TouchableOpacity>
+                    <Text style={{left:20}}>Planning</Text>
+                </View>
+                <TouchableOpacity style={styles.circle_button}>
+                    <Feather name={"settings"}  size={60} color="#01a699" />
+                </TouchableOpacity>
             </View>
             <View style={{ flexDirection: "row", paddingBottom:50 }}>
-            <View style={{paddingRight:50}}>
-            <TouchableOpacity
-                style={{
-                    borderWidth:1,
-                    borderColor:'rgba(0,0,0,0.2)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:100,
-                    height:100,
-                    backgroundColor:'#fff',
-                    borderRadius:50,
-                }}>
-                <Icon name={"chevron-right"}  size={30} color="#01a699" />
-            </TouchableOpacity>
-            </View>
-            <TouchableOpacity
-                style={{
-                    borderWidth:1,
-                    borderColor:'rgba(0,0,0,0.2)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:100,
-                    height:100,
-                    backgroundColor:'#fff',
-                    borderRadius:50
-                }}>
-                <Icon name={"chevron-right"}  size={30} color="#01a699" />
-            </TouchableOpacity>
+                <View style={{paddingRight:50}}>
+                    <TouchableOpacity style={styles.circle_button}>
+                        <FontAwesome name={"chevron-right"}  size={30} color="#01a699" />
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={styles.circle_button}>
+                    <FontAwesome name={"chevron-right"}  size={30} color="#01a699" />
+                </TouchableOpacity>
             </View>
             <View style={{ flexDirection: "row", paddingBottom:50 }}>
-            <View style={{paddingRight:50}}>
-            <TouchableOpacity
-                style={{
-                    borderWidth:1,
-                    borderColor:'rgba(0,0,0,0.2)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:100,
-                    height:100,
-                    backgroundColor:'#fff',
-                    borderRadius:50,
-                }}>
-                <Icon name={"chevron-right"}  size={30} color="#01a699" />
-            </TouchableOpacity>
+                <View style={{paddingRight:50}}>
+                    <TouchableOpacity style={styles.circle_button}>
+                        <FontAwesome name={"chevron-right"}  size={30} color="#01a699" />
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={styles.circle_button}>
+                    <FontAwesome name={"chevron-right"}  size={30} color="#01a699" />
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity
-                style={{
-                    borderWidth:1,
-                    borderColor:'rgba(0,0,0,0.2)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:100,
-                    height:100,
-                    backgroundColor:'#fff',
-                    borderRadius:50
-                }}>
-                <Icon name={"chevron-right"}  size={30} color="#01a699" />
-            </TouchableOpacity>
-            </View>
-            
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    circle_button: {
+        borderWidth:1,
+        borderColor:'rgba(0,0,0,0.2)',
+        alignItems:'center',
+        justifyContent:'center',
+        width:100,
+        height:100,
+        backgroundColor:'#fff',
+        borderRadius:50
+    }
+})
 
 export default HomeScreen;
