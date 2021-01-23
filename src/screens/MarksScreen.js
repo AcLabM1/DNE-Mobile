17547fd65947b7c70b735a424ffb0ca1b80b64a8
Tristan <mytic12@hotmail.fr>
2021-01-23 19:54:00 +0100

    update page note

--- src/screens/MarksScreen.js
+++ src/screens/MarksScreen.js
@@ -20,14 +20,103 @@ const MarksScreen = ({ navigation }) => {
     getValue('user_id');
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Bonjour {getUsernameByID(userID)}</Text>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <View style={styles.info_titles}>
+                <Text style={styles.title_content}>Cours</Text>
+                <Text style={styles.title2_content}>Coef.</Text>
+                <Text style={styles.title2_content}>Moy.</Text>
+                <Text style={styles.title2_content}>Note</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>Anglais</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>Programmation par Composant</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>Programmation fonctionelle</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>Cybersécurité</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>IA</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>Droit & Données</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
+            <View style={styles.info_marks}>
+                <Text style={styles.marks_content}>Ac'Lab</Text>
+                <Text style={styles.marks2_content}>*coef*</Text>
+                <Text style={styles.marks2_content}>*moy*</Text>
+                <Text style={styles.marks2_content}>*note*</Text>
+            </View>
         </View>
-    );
+);
 };
 
 const styles = StyleSheet.create({
+info_marks: {
+    marginTop: 10,
+    flexDirection: 'row',
+    backgroundColor: 'rgba(43,153,206,0.2)',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    fontSize: 5
+},
 
+info_titles: {
+    marginTop: 10,
+    flexDirection: 'row',
+    backgroundColor: '#2b99ce',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    fontSize: 5
+},
+
+title_content:{
+    color: '#ffffff',
+    width: '40%',
+    paddingLeft: 10
+},
+
+title2_content:{
+    color: '#ffffff',
+    width: '20%',
+    paddingLeft: 10
+},
+
+marks_content: {
+  color: 'grey',
+  width: '40%',
+  paddingLeft: 10
+},
+
+marks2_content: {
+    color: 'grey',
+    width: '20%',
+    paddingLeft: 10
+  }
 })
 
 export default MarksScreen;
